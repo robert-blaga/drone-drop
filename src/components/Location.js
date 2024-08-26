8766d89210273bdd8baeab78b5c683243c22ff9c
Robert Blaga <robertblaga@Roberts-MacBook-Pro.local>
2024-08-26 20:34:41 +0300

    Update drone drop game

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,13 +1,22 @@
 import React from 'react';
-// import SuccessIcon from './SuccessIcon';
 import '../styles/Location.css';
 
 const Location = ({ type, success }) => {
+  const titles = {
+    city: 'City',
+    smallBusiness: 'Small business',
+    largeOffice: 'Corporation',
+    village: 'Remote village',
+    oilRig: 'Oil rig',
+  };
+
   return (
     <div className={`location ${type}`}>
-      <div className={`location-circle ${success ? 'success' : ''}`}>
+      <div className="location-circle">
+        {/* Existing SVG or other content */}
         <img src={`${process.env.PUBLIC_URL}/assets/${type}.png`} alt={type} />
       </div>
+      <div className={`neon-text ${type}`}>{titles[type]}</div> {/* Changed from <a> to <div> */}
     </div>
   );
 };
